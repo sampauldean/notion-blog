3721e1ab4e0ee91617f25179763ae227d0637b2e
samuel-dean-machine <sam.pauldean@gmail.com>
2023-02-26 14:53:49 -0600

    Image call fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: [
+      'www.notion.so',
+      'lh3.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      's3-us-west-2.amazonaws.com'
+    ],
   },
 }
